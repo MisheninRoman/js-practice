a6b3a3237276887101d0ab3b7b5c33b2872e21d7
MisheninRoman <misheninrb@yandex.ru>
2023-03-03 16:13:29 +0300

    third changes in practice

--- js/practic.js
+++ js/practic.js
@@ -1,6 +1,16 @@
 "use strict";
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+let numberOfFilms;
+
+function start() {
+  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+  }
+}
+
+start();
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -10,29 +20,36 @@ const personalMovieDB = {
   private: false,
 };
 
-for (let i = 0; i < 2; i++) {
-  const a = prompt("Последний просмотренный фильм?", ""),
-    b = +prompt("На сколько его оцените?", "");
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt("Последний просмотренный фильм?", ""),
+      b = +prompt("На сколько его оцените?", "");
 
-  if (a != null && b != null && a != "" && b != "" && a.length < 51) {
-    personalMovieDB.movies[a] = b;
-    console.log("done");
-  } else {
-    console.log("FU");
-    i--;
+    if (a != null && b != null && a != "" && b != "" && a.length < 51) {
+      personalMovieDB.movies[a] = b;
+      console.log("done");
+    } else {
+      console.log("FU");
+      i--;
+    }
   }
 }
 
-if (personalMovieDB.count < 10 && personalMovieDB.count > 0) {
-  console.log("слишком мало");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-  console.log("не плохо, но можно и лучше");
-} else if (personalMovieDB.count >= 30) {
-  console.log("да вы человек голубых кровей");
-} else if (personalMovieDB.count < 1) {
-  console.log("ты кажется ошибся!");
-} else {
-  console.log("error");
+rememberMyFilms();
+
+function detecPersonalLvl() {
+  if (personalMovieDB.count < 10 && personalMovieDB.count > 0) {
+    console.log("слишком мало");
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("не плохо, но можно и лучше");
+  } else if (personalMovieDB.count >= 30) {
+    console.log("да вы человек голубых кровей");
+  } else if (personalMovieDB.count < 1) {
+    console.log("ты кажется ошибся!");
+  } else {
+    console.log("error");
+  }
 }
+detecPersonalLvl();
 
 console.log(personalMovieDB);
