fa36405feb18f94ca5b4661dc800ca640bf4a385
MisheninRoman <misheninrb@yandex.ru>
2023-04-04 16:00:24 +0300

    moreChanges

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
 "use strict";
 
-const box = document.getElementById();
+const box = document.getElementById("box"),
+  btn = document.getElementsByTagName("button"),
+  radius = 15;
+
+box.style.backgroundColor = "red";
+box.style.cssText = `background-color: blue; border-radius: ${radius}px`;
+
+// btn[0].style.borderRadius = `${radius}px`;
+// btn[0].style.border = "1px solid crimson";
+// btn[0].style.backgroundColor = "crimson";
+
+// btn[1].style.borderRadius = `${radius}px`;
+// btn[1].style.border = "1px solid blue";
+// btn[1].style.backgroundColor = "blue";
+
+// btn[2].style.borderRadius = `${radius}px`;
+// btn[2].style.border = "1px solid blueviolet";
+// btn[2].style.backgroundColor = "blueviolet";
+
+// for (let i = 0; i < btn.length; i++) {
+//   btn[i].style.cssText =
+//     "background-color: red; border-radius: 15px; border: 1px solid red";
+// }
